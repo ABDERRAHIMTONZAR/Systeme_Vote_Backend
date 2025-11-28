4efb63ed5d6f2eafe51e11bf11ee6aa6d6a678b5
Abderrahim Tonzar <abderrahim.tonzar@gmail.com>
2025-11-28 01:18:20 +0100

    Ajout des routes de sondages

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const cors = require('cors');
 app.use(cors());
 let indexRouter = require('./routes/index');
 let autroutes = require('./routes/auth.routes');
- 
+ let sondageRoutes = require('./routes/sondageRoutes');
  
 
 
 app.use('/', indexRouter);
 app.use('/users', autroutes);
-
+app.use('/sondage', sondageRoutes);
 app.listen(3001, () => {
   console.log('Example app listening on port 3001!')
 })

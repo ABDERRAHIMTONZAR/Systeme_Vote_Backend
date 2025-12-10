d610206291279a9c5205035002afe8a785a0c4bf
Abderrahim Tonzar <abderrahim.tonzar@gmail.com>
2025-12-10 23:47:56 +0100

    Ajout de quelques routes dashbord

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -5,6 +5,9 @@ const { getMonthlyStats } = require("../controllers/dashboardController");
 const { getPollStatusDistribution } = require("../controllers/dashboardController");
 const { getVoterEngagementDistribution } = require("../controllers/dashboardController");
 const { createPoll } = require("../controllers/dashboardController");
+const { getAllPolls } = require("../controllers/dashboardController");
+const { updatePoll } = require("../controllers/dashboardController");
+const { deletePoll } = require("../controllers/dashboardController");
 const {auth} = require("../middleware/auth");
 
 router.get("/stats", auth, getDashboardStats);
@@ -13,5 +16,8 @@ router.get("/monthly-stats",auth, getMonthlyStats);
 router.get("/poll-status", auth,getPollStatusDistribution);
 router.get("/engagement", auth, getVoterEngagementDistribution);
 router.post("/create-poll", auth, createPoll);
+router.get("/my-polls", auth, getAllPolls);
+router.put("/update/:id", auth, updatePoll);
+router.delete("/delete/:id", auth, deletePoll);
 
 module.exports = router;

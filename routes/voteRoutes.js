f5f8dd41d413e101bffb2ae7dcd3b69c945b309e
Ali Belaouali <ali.belaouali@example.com>
2025-12-31 00:13:07 +0100

    CommitAliPageMyPollsVotes

--- routes/voteRoutes.js
+++ routes/voteRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const voteController = require("../controllers/voteController");
 const { auth } = require("../middleware/auth");
 router.post("/insert", auth, voteController.insertVote);
-
+router.get('/votes',auth, voteController.getVotesByPoll); 
 
 module.exports = router;
